be389789aca12b97e4d5ac183c1953b908c3a953
Catherine Stoll <KateMstoll@gmail.com>
2017-07-07 21:08:14 -0700

    Assignment 20:complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,17 @@ require 'random_data'
    )
  end
 
+ 100.times do
+  Question.create!(
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  resolve: false
+  )
+end
+
+
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Question.count} questions created"
